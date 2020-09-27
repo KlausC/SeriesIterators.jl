41b90159022431b621bdb8cd9f16e056589801cf
Klaus Crusius <klaus.crusius@web.de>
2020-09-27 18:55:56 +0200

    initial code

--- src/SeriesIterators.jl
+++ src/SeriesIterators.jl
@@ -1,5 +1,32 @@
 module SeriesIterators
 
-# Write your package code here.
+export TermIterator
+
+using IterTools
+using Base.Iterators
+
+import Base: iterate, IteratorSize, length, size
+
+struct TermIterator{F,I}
+    generate::F
+    indices::I
+end
+function iterate(t::TermIterator)
+    vs = iterate(t.indices)
+    k, s = @IterTools.ifsomething vs
+    v = t.generate(k)
+    v[1], (s, v)
+end
+function iterate(t::TermIterator, s)
+    sx, p = s
+    vs = iterate(t.indices, sx)
+    k, s = @IterTools.ifsomething vs
+    v = t.generate(k, p)
+    v[1], (s, v)
+end
+IteratorSize(::Type{<:TermIterator{<:Any,I}}) where I = IteratorSize(I)
+length(t::TermIterator) = length(t.indices)
+size(t::TermIterator) = size(t.indices)
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
 using SeriesIterators
 using Test
 
+# Test: Ramanujan's famous series to calculate 1/π
+# https://en.wikipedia.org/wiki/Srinivasa_Ramanujan#Mathematical_achievements
+ramagen(k) = big(1103.0) * 2 * sqrt(big(2)) / 9801, big(1.0)
+function ramagen(k, (p,p1))
+    k = oftype(p1, k)
+    p1 *= (1-3/4k)*(1-2/4k)*(1-1/4k)/99^4
+    p = p1 * (1103 + 26390*k) * 2 * sqrt(oftype(p1, 2)) / 9801
+    p, p1
+end
+
 @testset "SeriesIterators.jl" begin
-    # Write your tests here.
+
+    setprecision(BigFloat, 256)
+    t = TermIterator(ramagen, 0:5)
+    @test sum(abs, (cumsum(t) .- big(1)/pi ) .* exp10.(8:8:48)) < 10
+    @test inv(cumsum(t)[end]) ≈ pi
+
 end
