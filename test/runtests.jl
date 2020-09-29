82683275789aa11176f7c3bd989ed729e861f9ad
Klaus Crusius <klaus.crusius@web.de>
2020-09-29 19:41:38 +0200

    Bernoulli numbers added

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,9 +13,30 @@ end
 
 @testset "SeriesIterators.jl" begin
 
+@testset "term iterator" begin
     setprecision(BigFloat, 256)
-    t = TermIterator(ramagen, 0:5)
+    t = iterm(ramagen, 0:5)
     @test sum(abs, (cumsum(t) .- big(1)/pi ) .* exp10.(8:8:48)) < 10
     @test inv(cumsum(t)[end]) ≈ pi
+end
+
+@testset "bernoulli" begin
+    @test bernoulli(0) == 1
+    @test bernoulli(1) == -1//2
+    @test bernoulli(2) == 1//6
+    @test bernoulli(3) == 0
+    @test bernoulli.(4:2:20) == [
+      -1//30
+       1//42
+      -1//30
+       5//66
+    -691//2730
+       7//6
+   -3617//510
+   43867//798
+   -174611//330]
+    @test bernoulli(200) == -498384049428333414764928632140399662108495887457206674968055822617263669621523687568865802302210999132601412697613279391058654527145340515840099290478026350382802884371712359337984274122861159800280019110197888555893671151//1366530
+
+end
 
 end
